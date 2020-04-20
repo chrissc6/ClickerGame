72b45a168b7709cdbdde5e7388e7498b20e74ece
chris <christopherscaudill@gmail.com>
2020-04-20 19:03:06 -0400

    ic - button and label

---
+++ ClickerGame/Assets/IdleTut.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IdleTut : MonoBehaviour
+{
+    public Text cashText;
+    public double cash;
+
+    public void Start()
+    {
+        cash = 0;
+    }
+
+    public void Update()
+    {
+        cashText.text = "Cash: $" + cash;
+    }
+
+    public void Work()
+    {
+        cash++;
+    }
+
+
+
+
+
+
+
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
