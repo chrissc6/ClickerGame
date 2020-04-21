17215192202dde77161836bfecdda9ed9c7b85a1
chris <christopherscaudill@gmail.com>
2020-04-20 20:41:41 -0400

    added biz, and click upgrade

--- ClickerGame/Assets/IdleTut.cs
+++ ClickerGame/Assets/IdleTut.cs
@@ -7,22 +7,68 @@ public class IdleTut : MonoBehaviour
 {
     public Text cashText;
     public double cash;
+    public double cashClickValue;
+
+    public Text cashPerSecText;
+    public double cashPerSec;
+
+    public Text workUpgradeText;
+    public double workUpgradeCost;
+    public double workUpgradeLevel;
+
+    public Text biz1Text;
+    public double biz1Cost;
+    public double biz1UpgradeLevel;
 
     public void Start()
     {
-        cash = 0;
+        cashClickValue = 1;
+        workUpgradeCost = 10;
+        biz1Cost = 25;
+        biz1UpgradeLevel = 1;
+        workUpgradeLevel = 1;
     }
 
     public void Update()
     {
-        cashText.text = "Cash: $" + cash;
+        //cashPerSec = biz1UpgradeLevel;
+
+        cashText.text = "Cash: $" + cash.ToString("F0");
+        cashPerSecText.text = "Cash Per Sec: $" + cashPerSec.ToString("F0");
+        workUpgradeText.text = "Upgrade: $" + workUpgradeCost.ToString("F0") + " \nROI: +$" + workUpgradeLevel + "per click";
+        biz1Text.text = "Lemonade Stand $" + biz1Cost.ToString("F0") + " \nROI: +$" + biz1UpgradeLevel + " cash /sec";
+
+        cash += cashPerSec * Time.deltaTime;
     }
 
     public void Work()
     {
-        cash++;
+        cash += cashClickValue;
     }
 
+    public void WorkUpgrade()
+    {
+        if (cash >= workUpgradeCost)
+        {
+            cash -= workUpgradeCost;
+            cashClickValue += workUpgradeLevel;
+
+            workUpgradeLevel++;
+            workUpgradeCost *= 2;
+        }
+    }
+
+    public void Biz1()
+    {
+        if (cash >= biz1Cost)
+        {
+            cash -= biz1Cost;
+            cashPerSec += biz1UpgradeLevel;
+
+            biz1UpgradeLevel++;
+            biz1Cost += 5;
+        }
+    }
 
 
 
